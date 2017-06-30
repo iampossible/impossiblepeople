45fee96bb3d4b5e8b284704b90f462e526467d47
Susana Cardoso <Susana@impossibles-MacBook-Pro.local>
2017-06-30 16:17:19 +0100

    Finished search on explore api.

--- api/src/models/ExploreModel.js
+++ api/src/models/ExploreModel.js
@@ -21,6 +21,24 @@ class ExploreModel extends Model {
       );
     });
   }
+  search(interest, keyword){
+    return new Sequence((accept, reject) => {
+      this.db.query(
+        `MATCH (creator:Person) -[rel:OFFERS|:ASKS]-> (post:Post) -[:IS_ABOUT]-> (category:Interest{name:{interest}}), (user:Person)
+         WHERE post.content =~ {keyword}
+         OPTIONAL MATCH (post) <-[comments:COMMENTS]- (:Person)
+         RETURN creator, rel, post, category,
+            COUNT( DISTINCT comments) AS commentCount,
+            [] AS commonFriends    
+         ORDER BY rel.at DESC`,
+        { interest, keyword},
+        (err, response) => {
+          if (err) return reject(err);
+          return accept(response);
+        }
+      );
+    });
+  }
 }
 
 module.exports = new ExploreModel();
