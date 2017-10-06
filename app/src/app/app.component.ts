27fced7f95223d61b31a88bc5b7d7d92eff6f7a7
Filipe Gonçalves <filipe@impossible.com>
2017-10-06 12:29:18 +0100

    Small fixes

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -37,8 +37,8 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      //statusBar.styleDefault();
-      statusBar.styleBlackTranslucent();
+      statusBar.styleDefault();
+      //statusBar.styleBlackTranslucent();
       splashScreen.hide();
 
       try {
