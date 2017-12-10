6bd2ab56ed6e74e037f6c38d660adb8edcfdf04a
KiyaG <hamilee.kiya@gmail.com>
2017-12-10 15:12:08 +0000

    handled the facebook login cancel

--- web/src/components/views/LandingPage.js
+++ web/src/components/views/LandingPage.js
@@ -11,45 +11,43 @@ export default class LandingPage extends Component {
     };
   }
   responseFacebook = response => {
-    this.setState(
-      {
-        loading: true
-      },
-      () => {
-        //validate the token
-        return (
-          fetch(`/api/facebook/check?token=${response.accessToken}`, {
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json"
-            },
-            //must be set else for next request you will get 401:unauthorised
-            credentials: "same-origin"
-          })
-            //if it is valid create the user and return some of his/her data
-            .then(response => response.json())
-            .then(response => {
-              let user = response;
-              this.props.setUser(user);
-              if (
-                user &&
-                user.hasOwnProperty("userType") &&
-                user.userType === ""
-              ) {
-                this.props.history.push("/interest");
-              }
-              if (
-                user &&
-                user.userType && 
-                user.userType !== ""
-                ) {
-                this.props.history.push("/feed");
-              }
+    if (response.status !== "unknown") {
+      this.setState(
+        {
+          loading: true
+        },
+        () => {
+          //validate the token
+          return (
+            fetch(`/api/facebook/check?token=${response.accessToken}`, {
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+              },
+              //must be set else for next request you will get 401:unauthorised
+              credentials: "same-origin"
             })
-            .catch(err => console.log(err))
-        );
-      }
-    );
+              //if it is valid create the user and return some of his/her data
+              .then(response => response.json())
+              .then(response => {
+                let user = response;
+                this.props.setUser(user);
+                if (
+                  user &&
+                  user.hasOwnProperty("userType") &&
+                  user.userType === ""
+                ) {
+                  this.props.history.push("/interest");
+                }
+                if (user && user.userType && user.userType !== "") {
+                  this.props.history.push("/feed");
+                }
+              })
+              .catch(err => console.log(err))
+          );
+        }
+      );
+    }
   };
 
   render() {
@@ -60,7 +58,7 @@ export default class LandingPage extends Component {
           <Col className="sm-6">
             {!this.state.loading ? (
               <FacebookLogin
-                appId="138462666798513"
+                appId="133088487346292"
                 autoLoad={false}
                 icon="fa-facebook fa-lg"
                 fields="name,email,picture,friends"
