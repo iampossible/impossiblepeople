d46b91625fe9ad307f1654c77dee1cfe594885cc
KiyaG <hamilee.kiya@gmail.com>
2018-01-06 15:21:11 +0000

    made the footer component responsive

--- web/src/components/views/Footer.js
+++ web/src/components/views/Footer.js
@@ -4,24 +4,25 @@ import { Container, Row, Col } from "reactstrap";
 export default class Footer extends Component {
   render() {
     return (
-      <Container>
+      <Container id="footer">
         <Row>
-          <Col xs={4} />
-          <Col xs={2}>
-            <p id="termsOfUse">
+          <Col sm={4} className="d-none d-sm-block" />
+          <Col xs={4} sm={2} id="termsOfUse">
+            <p>
               <a href="#">Terms of user</a>
             </p>
           </Col>
-          <Col xs={2}>
-            <p id="privacyPlicy">
+          <Col xs={5} sm={2} id="privacyPlicy">
+            <p>
               <a href="#">Privacy Policy</a>
             </p>
           </Col>
-          <Col xs={2} />
-          <Col xs={2}>
-            <p id="footerCopyright">
-              <span>We Are One</span>&nbsp;
-              <i className="fa fa-copyright" aria-hidden="true" /> 2018
+          <Col sm={1} className="d-none d-sm-block" />
+          <Col xs={3} sm={3} id="footerCopyright">
+            <p>
+              <span className="d-none d-sm-inline">We Are One</span>&nbsp;
+              <i className="fa fa-copyright" aria-hidden="true" />&nbsp;
+              {new Date().getFullYear()}
             </p>
           </Col>
         </Row>
