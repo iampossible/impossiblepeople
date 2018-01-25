55b4eeac41036ba303f4c974dbe082ab3ef51e34
Filipe Gonçalves <filipe@impossible.com>
2018-01-25 17:54:46 +0000

    Do not request deprecated bio field on facebook's graph api

--- api/src/middleware/FacebookService.js
+++ api/src/middleware/FacebookService.js
@@ -44,7 +44,7 @@ class FacebookService {
 
   getUserDetails(facebookUserID) {
     return new Sequence((accept, reject) => {
-      let fields = 'email,bio,first_name,last_name,picture.type(large),friends';
+      let fields = 'email,about,first_name,last_name,picture.type(large),friends';
       let facebookUserEndpoint = `${baseUrl}/${facebookUserID}?access_token=${this.accessToken}&fields=${fields}`;
       console.debug('FacebookService@getUserDetails:', `get: ${facebookUserEndpoint}`);
 
@@ -64,7 +64,7 @@ class FacebookService {
                   email: facebookData.email,
                   firstName: facebookData.first_name,
                   lastName: facebookData.last_name,
-                  biography: facebookData.bio,
+                  biography: facebookData.about,
                   imageSource: `https://graph.facebook.com/${facebookData.id}/picture?type=large`,
                 },
               });
