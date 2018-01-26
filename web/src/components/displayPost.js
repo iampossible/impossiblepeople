0d0fafe54a3a4f31eb090304ffa3ecdf41a73016
KiyaG <hamilee.kiya@gmail.com>
2018-01-26 15:22:41 +0000

    included a default picture for the feed component to associate with the organisation that posted, if no image has been associated with it

--- web/src/components/displayPost.js
+++ web/src/components/displayPost.js
@@ -2,162 +2,142 @@ import React, { Component } from "react";
 import { Row, Col, Button, Tooltip } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import * as moment from "moment";
+import currentUserAvatar from "../assets/images/profile.png";
 
 class DisplayPost extends Component {
-    state = {
-        updateTooltipOpen: false,
-        deleteTooltipOpen: false,
-    };
+  state = {
+    updateTooltipOpen: false,
+    deleteTooltipOpen: false
+  };
 
-    toggleUpdatePost = () => {
-        this.setState({
-            updateTooltipOpen: !this.state.updateTooltipOpen
-        });
-    };
+  toggleUpdatePost = () => {
+    this.setState({
+      updateTooltipOpen: !this.state.updateTooltipOpen
+    });
+  };
 
-    toggleDeletePost = () => {
-        this.setState({
-            deleteTooltipOpen: !this.state.deleteTooltipOpen
-        });
-    };
+  toggleDeletePost = () => {
+    this.setState({
+      deleteTooltipOpen: !this.state.deleteTooltipOpen
+    });
+  };
 
-    render() {
-        const postData = this.props.postData;
-        const user = this.props.user;
-        return ( 
-          <Row xs={12}> 
-            <Col sm={3} xs={12}> 
-              <Row className="feedPhoto">
-                <img
-                  className="img-fluid feedPhoto"
-                  src={
-                    postData.author.imageSource ||
-                    "../assets/images/profile-icon.png"
-                  }
-                  alt="profile"
-                />
-                <blockquote className="blockquote">
-                  <footer className="blockquote-footer">
-                    <span className="feedAuthor">
-                      <i
-                        className="fa fa-sm fa-chevron-right"
-                        aria-hidden="true"
-                      />
-                      &nbsp;
-                      {postData.author.username}
-                    </span>&nbsp;
-                  </footer>
-                </blockquote>
-              </Row>
-            </Col>
-            <Col className="feedBody" sm={9} xs={12}>
-              <Row>
-                <Col xs={12}>
-                  {postData.author.userID === user.userID ? (
-                    <Row xs={12}>
-                      <Col xs={10} />
-                      <Col
-                        xs={2}
-                        className="updateDeletePostButtonsContainer"
-                      >
-                        <a
-                          id="ToolTipUpdateIcon"
-                          className="updatePostIcon"
-                          href="#post"
-                          onClick={() =>
-                            this.props.handlePostUpdate(postData.postID)
-                          }
-                        >
-                          <i className="material-icons">edit</i>
-                        </a>
-                        <Tooltip
-                          placement="left"
-                          isOpen={this.state.updateTooltipOpen}
-                          target="ToolTipUpdateIcon"
-                          toggle={this.toggleUpdatePost}
-                        >
-                          Update Post
-                        </Tooltip>
-                        <a
-                          id="TooltipDeleteIcon"
-                          className="deletePostIcon"
-                          onClick={() =>
-                            this.props.handlePostDelete(postData.postID)
-                          }
-                        >
-                          <i className="material-icons">clear</i>
-                        </a>
-                        <Tooltip
-                          placement="right"
-                          isOpen={this.state.deleteTooltipOpen}
-                          target="TooltipDeleteIcon"
-                          toggle={this.toggleDeletePost}
-                        >
-                          Delete Post
-                        </Tooltip>
-                      </Col>
-                    </Row>
-                  ) : null}
-                </Col>
-              </Row>
-              <Row> 
-                <Col xs={12}>
-                  <blockquote className="blockquote">
-                    <p className="feedContent">{postData.content}</p>
-                    <footer className="blockquote-footer">
-                      <cite title="Source Title">
-                        <Row>
-                          <Col xs={1} className="feedTagsIcon">
-                            <span>
-                              <i
-                                className="fa fa-sm fa-tag"
-                                aria-hidden="true"
-                              />
-                            </span>
-                          </Col>
-                          <Col xs={11}>
-                            <p className="feedTags">
-                              {/* if the post has more than one tag/interest */}
-                              <span>
-                                {postData.interests.map(interest => interest.name).join(" / ")}
-                              </span>
-                            </p>
-                          </Col>
-                          <Col xs={1} className="feedLocationIcon">
-                            <span>
-                              <i
-                                className="fa fa-map-marker"
-                                aria-hidden="true"
-                              />
-                            </span>
-                          </Col>
-                          <Col xs={11}>
-                            <p className="feedLocation">
-                              {postData.location}
-                            </p>
-                          </Col>
-                          <Col xs={1} className="feedCreatedAtIcon">
-                            <span>
-                              <i className="fa fa-calendar" />
-                            </span>
-                          </Col>
-                          <Col xs={11}>
-                            <p className="feedCreatedAt">
-                              {moment(postData.createdAt).format(
-                                "MMM Do, YYYY"
-                              )}&nbsp;
-                            </p>
-                          </Col>
-                        </Row>
-                      </cite>
-                    </footer>
-                  </blockquote>
-                </Col>
-              </Row>
+  render() {
+    const postData = this.props.postData;
+    const user = this.props.user;
+    return (
+      <Row xs={12}>
+        <Col sm={3} xs={12}>
+          <Row className="feedPhoto">
+            <img
+              className="img-fluid feedPhoto"
+              src={postData.author.imageSource || currentUserAvatar}
+              alt="profile"
+            />
+            <blockquote className="blockquote">
+              <footer className="blockquote-footer">
+                <span className="feedAuthor">
+                  <i className="fa fa-sm fa-chevron-right" aria-hidden="true" />
+                  &nbsp;
+                  {postData.author.username}
+                </span>&nbsp;
+              </footer>
+            </blockquote>
+          </Row>
+        </Col>
+        <Col className="feedBody" sm={9} xs={12}>
+          <Row>
+            <Col xs={12}>
+              {postData.author.userID === user.userID ? (
+                <Row xs={12}>
+                  <Col xs={10} />
+                  <Col xs={2} className="updateDeletePostButtonsContainer">
+                    <a
+                      id="ToolTipUpdateIcon"
+                      className="updatePostIcon"
+                      href="#post"
+                      onClick={() =>
+                        this.props.handlePostUpdate(postData.postID)
+                      }>
+                      <i className="material-icons">edit</i>
+                    </a>
+                    <Tooltip
+                      placement="left"
+                      isOpen={this.state.updateTooltipOpen}
+                      target="ToolTipUpdateIcon"
+                      toggle={this.toggleUpdatePost}>
+                      Update Post
+                    </Tooltip>
+                    <a
+                      id="TooltipDeleteIcon"
+                      className="deletePostIcon"
+                      onClick={() =>
+                        this.props.handlePostDelete(postData.postID)
+                      }>
+                      <i className="material-icons">clear</i>
+                    </a>
+                    <Tooltip
+                      placement="right"
+                      isOpen={this.state.deleteTooltipOpen}
+                      target="TooltipDeleteIcon"
+                      toggle={this.toggleDeletePost}>
+                      Delete Post
+                    </Tooltip>
+                  </Col>
+                </Row>
+              ) : null}
             </Col>
           </Row>
-        );
-    }
+          <Row>
+            <Col xs={12}>
+              <blockquote className="blockquote">
+                <p className="feedContent">{postData.content}</p>
+                <footer className="blockquote-footer">
+                  <cite title="Source Title">
+                    <Row>
+                      <Col xs={1} className="feedTagsIcon">
+                        <span>
+                          <i className="fa fa-sm fa-tag" aria-hidden="true" />
+                        </span>
+                      </Col>
+                      <Col xs={11}>
+                        <p className="feedTags">
+                          {/* if the post has more than one tag/interest */}
+                          <span>
+                            {postData.interests
+                              .map(interest => interest.name)
+                              .join(" / ")}
+                          </span>
+                        </p>
+                      </Col>
+                      <Col xs={1} className="feedLocationIcon">
+                        <span>
+                          <i className="fa fa-map-marker" aria-hidden="true" />
+                        </span>
+                      </Col>
+                      <Col xs={11}>
+                        <p className="feedLocation">{postData.location}</p>
+                      </Col>
+                      <Col xs={1} className="feedCreatedAtIcon">
+                        <span>
+                          <i className="fa fa-calendar" />
+                        </span>
+                      </Col>
+                      <Col xs={11}>
+                        <p className="feedCreatedAt">
+                          {moment(postData.createdAt).format("MMM Do, YYYY")}&nbsp;
+                        </p>
+                      </Col>
+                    </Row>
+                  </cite>
+                </footer>
+              </blockquote>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    );
+  }
 }
 
 export default DisplayPost;
