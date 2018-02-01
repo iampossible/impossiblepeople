0116ad8f4f635d6d207b2c477ea8d2e34b136ff1
KiyaG <hamilee.kiya@gmail.com>
2018-02-01 12:58:30 +0000

    implemented the buildOrgProfile component for building organisations profile

--- web/src/components/views/BuildOrgProfile.js
+++ web/src/components/views/BuildOrgProfile.js
@@ -10,7 +10,6 @@ import {
   Input
 } from "reactstrap";
 import Interest from "./Interest";
-import defaultProfileImage from "../../assets/images/profile.png";
 import { RingLoader } from "react-spinners";
 
 function getBase64(file) {
@@ -21,24 +20,63 @@ function getBase64(file) {
     reader.onerror = error => reject(error);
   });
 }
+function handleErrors(response) {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response;
+}
 export default class BuildOrgProfile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "",
-      orgDescription: "",
-      orgUrl: "",
-      orgImage: defaultProfileImage,
-      interest: []
+      name: this.props.user.organisationName
+        ? this.props.user.organisationName
+        : "",
+      description: this.props.user.description
+        ? this.props.user.description
+        : "",
+      url: this.props.user.url ? this.props.user.url : "",
+      imageSource: this.props.user.imageSource
+        ? this.props.user.imageSource
+        : "",
+      interests: "",
+      uploadingImage: false
     };
   }
 
+  handleChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  //to handle the selection when the button is clicked
+  handleInterestSelection = evt => {
+    //get the interestID from the button selected/clicked
+    let interestID = evt.target.value;
+    let interests = new Set(this.state.interests);
+    if (interests.has(interestID)) {
+      interests.delete(interestID);
+      this.setState({ interests });
+      console.log("delete");
+    } else {
+      interests.add(interestID);
+      this.setState({ interests });
+      console.log("add");
+    }
+  };
+
   handleImageSelection = e => {
     e.persist();
-
     const files = e.target.files;
     const reader = new FileReader();
     const x = getBase64(files[0]).then(res => {
+      this.setState({
+        uploadingImage: true
+      });
       fetch(`/api/user/image`, {
         credentials: "same-origin",
         ContentType: "image/jpeg",
@@ -51,37 +89,89 @@ export default class BuildOrgProfile extends Component {
           return response.json();
         })
         .then(response => {
-          console.log(this.props.user);
           this.setState({
-            orgImage: response.imageSource
+            imageSource: response.imageSource,
+            uploadingImage: false
           });
         })
         .catch(err => console.log(err));
     });
   };
-  isPageReady = () => {
-    return this.props.user.imageSource;
+
+  handleSubmitRequest = e => {
+    fetch(`/api/user`, {
+      credentials: "same-origin",
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        organisationName: this.state.name,
+        description: this.state.description,
+        url: this.state.url
+      })
+    })
+      .then(response => response.json())
+      .then(response => {
+        this.updateUserInterest(response);
+      })
+      .catch(err => console.error(err));
+  };
+
+  updateUserInterest = user => {
+    fetch(`/api/user/interest`, {
+      credentials: "same-origin",
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        interests: [...this.state.interests]
+      })
+    })
+      //just for see the result of the operation...needs to be removed
+      .then(handleErrors)
+      .then(response => {
+        this.setState({
+          upladingImage: false
+        });
+        return response.json();
+      })
+      .then(response => {
+        //modify the value of the current user's interests
+        user.interests = response.interests;
+        this.redirectOnSubmit(user);
+      })
+      .catch(err => console.error(err));
+  };
+
+  redirectOnSubmit = user => {
+    this.props.setUser(user);
+    this.props.history.push("/feed");
+  };
+
+  isPageRady = () => {
+    return this.props.user.interests;
   };
 
   render() {
-    if (!this.isPageReady()) {
-      return (
-        <Row>
-          <Col xs={4} />
-          <Col xs={4} id="interestRingLoader">
-            <div className="RingLoader center-loading">
-              <RingLoader
-                color="#123abc"
-                loading={this.state.loading}
-                size={100} /*the size of the spinner*/
-              />
-            </div>
-          </Col>
-          <Col xs={4} />
-        </Row>
-      );
-    }
-    return (
+    return !this.isPageRady() ? (
+      <Row>
+        <Col xs={4} />
+        <Col xs={4} id="interestRingLoader">
+          <div className="RingLoader center-loading">
+            <RingLoader
+              color="#123abc"
+              loading={this.state.loading}
+              size={100} /*the size of the spinner*/
+            />
+          </div>
+        </Col>
+        <Col xs={4} />
+      </Row>
+    ) : (
       <Row id="profile">
         <Col sm={1} />
         <Col sm={10}>
@@ -92,15 +182,24 @@ export default class BuildOrgProfile extends Component {
               </Label>
               <Col sm={10}>
                 <Row>
-                  <Col sm={5}>
-                    <img
-                      id="preview"
-                      src={
-                        this.props.user.imageSource
-                          ? this.props.user.imageSource
-                          : this.state.orgImage
-                      }
-                    />
+                  <Col sm={5} id="profilePicture">
+                    {this.state.uploadingImage ? (
+                      <RingLoader
+                        id="ringLoader"
+                        color="#123abc"
+                        loading={this.state.loading}
+                        size={100} /*the size of the spinner*/
+                      />
+                    ) : (
+                      <img
+                        id="preview"
+                        src={
+                          this.state.imageSource !== ""
+                            ? this.state.imageSource
+                            : this.props.user.imageSource
+                        }
+                      />
+                    )}
                   </Col>
                   <Col sm={7} id="profilePictureInfo">
                     <FormText color="muted">
@@ -131,7 +230,12 @@ export default class BuildOrgProfile extends Component {
                   type="text"
                   name="name"
                   id="orgName"
-                  defaultValue={this.props.user.organisationName}
+                  value={
+                    this.state.name !== ""
+                      ? this.state.name
+                      : this.props.user.organisationName
+                  }
+                  onChange={this.handleChange}
                 />
               </Col>
               <Col sm={3} />
@@ -146,6 +250,10 @@ export default class BuildOrgProfile extends Component {
                   name="url"
                   id="orgUrl"
                   placeholder="your organisation url"
+                  value={
+                    this.state.url !== "" ? this.state.url : this.props.user.url
+                  }
+                  onChange={this.handleChange}
                 />
               </Col>
               <Col sm={3} />
@@ -155,7 +263,17 @@ export default class BuildOrgProfile extends Component {
                 Description
               </Label>
               <Col sm={10}>
-                <Input type="textarea" name="description" id="orgDescription" />
+                <Input
+                  type="textarea"
+                  name="description"
+                  id="orgDescription"
+                  value={
+                    this.state.description !== ""
+                      ? this.state.description
+                      : this.props.user.description
+                  }
+                  onChange={this.handleChange}
+                />
               </Col>
             </FormGroup>
             <FormGroup row>
@@ -164,6 +282,16 @@ export default class BuildOrgProfile extends Component {
                   user={this.props.user}
                   setUser={this.props.setUser}
                   getUser={this.props.getUser}
+                  handleInterestSelection={this.handleInterestSelection}
+                  interests={
+                    this.state.interests !== ""
+                      ? this.state.interests
+                      : new Set(
+                          this.props.user.interests.map(
+                            interest => interest.interestID
+                          )
+                        )
+                  }
                 />
               </Col>
             </FormGroup>
@@ -175,7 +303,7 @@ export default class BuildOrgProfile extends Component {
             <Row>
               <Col sm={4} />
               <Col sm={4}>
-                <Button color="danger" block>
+                <Button color="danger" block onClick={this.handleSubmitRequest}>
                   Submit
                 </Button>
               </Col>
