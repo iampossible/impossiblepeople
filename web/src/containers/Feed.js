300ac680b961053a0eb40203d8561c7b8bc343b4
KiyaG <hamilee.kiya@gmail.com>
2018-02-07 15:30:28 +0000

    moved all the states from the children components, DisplayPost and Comment, for ease of managing states

--- web/src/containers/Feed.js
+++ web/src/containers/Feed.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Row, Col } from "reactstrap";
 import { RingLoader } from "react-spinners";
 import Comment from "../components/Comment";
@@ -31,12 +31,43 @@ class Feed extends Component {
     postToUpdate: "",
     currentFilter: "MOSTRECENT",
     filterTag: "",
-    tagsDropdownOpen: false
+    tagsDropdownOpen: false,
+    newComment: "",
+    commentsDisplayed: false
   };
 
   componentWillMount() {
     this.getFeeds();
+    this.setState({
+      loadComment: false
+    });
+    this.forceUpdate();
   }
+
+  getComments = postID => {
+    fetch(`/api/post/${postID}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      credentials: "same-origin"
+    })
+      .then(resp => resp.json())
+      .then(resp => {
+        let feedDataClone = this.state.feed;
+        feedDataClone.map(feedData => {
+          if (postID === feedData.postID) {
+            feedData.comments = resp.comments;
+            feedData.commentCount = resp.comments.length;
+            this.setState({
+              feed: feedDataClone
+            });
+            this.forceUpdate();
+          }
+        });
+      });
+  };
 
   getFeeds = () => {
     this.setState({
@@ -56,12 +87,14 @@ class Feed extends Component {
         if (response.status > 399) return [];
         return response.json();
       })
-      .then(response =>
+      .then(response => {
         response.map(post => {
-          post.comments = [];
-          return post;
-        })
-      )
+          if (post.commentCount > 0) {
+            this.getComments(post.postID);
+          }
+        });
+        return response;
+      })
       .then(response => {
         this.setState({
           feed: response,
@@ -118,6 +151,37 @@ class Feed extends Component {
     });
   };
 
+  handleChange = event => {
+    // input is the Comment
+    this.setState({ newComment: event.target.value });
+  };
+
+  handleKeyUp = (e, postID) => {
+    const keyCode = e.keyCode;
+    //detecting enter key
+    if (keyCode === 13) {
+      console.log("made comment");
+      fetch(`/api/post/${postID}/comment`, {
+        method: "POST",
+        body: JSON.stringify({ content: this.state.newComment }),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        credentials: "same-origin"
+      }).then(() => {
+        this.getComments(postID);
+      });
+      this.setState(
+        {
+          newComment: ""
+        },
+        () => {
+          this.forceUpdate();
+        }
+      );
+    }
+  };
   render() {
     let counter = 0; // to track number of filtered results based on tag
     //getting the user type that is passed from the App redirect
@@ -137,7 +201,7 @@ class Feed extends Component {
         <Col xs={4} />
       </Row>
     ) : (
-      <div>
+      <Fragment>
         {/* if user is an organisation display the post component at the top */}
         {(user && (user.userType === "organisation" && user.approved)) ||
         user.admin ? (
@@ -149,65 +213,90 @@ class Feed extends Component {
         ) : (
           ""
         )}
-        <FilterButtons
-          interests={user.interests}
-          currentFilter={this.state.currentFilter}
-          filterTag={this.state.filterTag}
-          tagsDropdownOpen={this.state.tagsDropdownOpen}
-          toggleTagesDropdown={this.toggleTagesDropdown}
-          updateFilter={this.updateFilter}
-        />
-        {this.state.feed.length > 0 ? (
-          this.state.feed.map((feedData, i) => {
-            if (
-              (this.state.currentFilter === "ASKS" ||
-                this.state.currentFilter === "OFFERS") &&
-              feedData.postType !== this.state.currentFilter
-            )
-              return null;
-            if (
-              this.state.filterTag &&
-              !feedData.interests.reduce((acc, interest) => {
-                return acc || interest.interestID === this.state.filterTag;
-              }, false)
-            ) {
-              counter++;
-              if (counter > this.state.feed.length - 1) {
-                //if there is no feed related to the selected tag
-                return (
-                  <NoFeedMessage
-                    key={i}
-                    message=", which is related to the tag you have selected"
-                  />
-                );
-              } else {
-                return null;
-              }
-            }
+        <Row>
+          <Col sm={1} />
+          <Col sm={2}>
+            <FilterButtons
+              interests={user.interests}
+              currentFilter={this.state.currentFilter}
+              filterTag={this.state.filterTag}
+              tagsDropdownOpen={this.state.tagsDropdownOpen}
+              toggleTagesDropdown={this.toggleTagesDropdown}
+              updateFilter={this.updateFilter}
+            />
+          </Col>
 
-            return (
-              <div key={feedData.postID} className="feed">
-                <Row xs={12}>
-                  <DisplayPost
-                    postData={feedData}
-                    user={user}
-                    handlePostUpdate={this.handlePostUpdate}
-                    handlePostDelete={this.handlePostDelete}
-                  />
-                  <Row id="comments">
-                    <Col sm={3} />
-                    <Col sm={9}>
-                      <Comment postID={feedData.postID} user={user} />
-                    </Col>
-                  </Row>
-                </Row>
-              </div>
-            );
-          })
-        ) : (
-          <NoFeedMessage message="." />
-        )}
-      </div>
+          {this.state.feed.length > 0 ? (
+            <Col sm={8}>
+              {this.state.feed.map((feedData, i) => {
+                if (
+                  (this.state.currentFilter === "ASKS" ||
+                    this.state.currentFilter === "OFFERS") &&
+                  feedData.postType !== this.state.currentFilter
+                ) {
+                  counter++;
+                  if (counter > this.state.feed.length - 1) {
+                    //if there is no feed related to the selected tag
+                    return (
+                      <NoFeedMessage
+                        key={i}
+                        message=", which is related to the tag you have selected"
+                      />
+                    );
+                  } else {
+                    return null;
+                  }
+                }
+                if (
+                  this.state.filterTag &&
+                  !feedData.interests.reduce((acc, interest) => {
+                    return acc || interest.interestID === this.state.filterTag;
+                  }, false)
+                ) {
+                  counter++;
+                  if (counter > this.state.feed.length - 1) {
+                    //if there is no feed related to the selected tag
+                    return (
+                      <NoFeedMessage
+                        key={i}
+                        message=", which is related to the tag you have selected"
+                      />
+                    );
+                  } else {
+                    return null;
+                  }
+                }
+
+                return (
+                  <div key={feedData.postID} className="feed">
+                    <DisplayPost
+                      postData={feedData}
+                      user={user}
+                      handlePostUpdate={this.handlePostUpdate}
+                      handlePostDelete={this.handlePostDelete}
+                    />
+                    <Row id="comments">
+                      <Col sm={2} />
+                      <Col sm={9}>
+                        <Comment
+                          postID={feedData.postID}
+                          user={user}
+                          newComment={this.state.newComment}
+                          handleChange={this.handleChange}
+                          handleKeyUp={this.handleKeyUp}
+                        />
+                      </Col>
+                    </Row>
+                  </div>
+                );
+              })}
+            </Col>
+          ) : (
+            <NoFeedMessage message="." />
+          )}
+          <Col sm={1} />
+        </Row>
+      </Fragment>
     );
   }
 }
