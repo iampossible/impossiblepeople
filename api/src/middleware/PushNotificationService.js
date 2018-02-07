6627844fcda288735fc0eb195a337222fd33f6d6
KiyaG <hamilee.kiya@gmail.com>
2018-02-07 13:44:02 +0000

    implemented email sending feature when a comment is made on a post

--- api/src/middleware/PushNotificationService.js
+++ api/src/middleware/PushNotificationService.js
@@ -1,44 +1,48 @@
-'use strict';
+"use strict";
 
+const AWS = require("aws-sdk");
+const Sequence = require("impossible-promise");
 
-const AWS = require('aws-sdk');
-const Sequence = require('impossible-promise');
-
-const config = require('config/server');
-const userModel = require('models/UserModel');
+const config = require("config/server");
+const userModel = require("models/UserModel");
 
 const sns = new AWS.SNS({
   accessKeyId: config.aws.accessKey,
   logger: console.info,
-  region: 'eu-west-1',
+  region: "eu-west-1",
   secretAccessKey: config.aws.secretKey,
-  sslEnabled: true,
+  sslEnabled: true
 });
 const appleApplicationArn = config.aws.sns.Apple;
 const androidApplicationArn = config.aws.sns.Android;
 
 class PushNotificationService {
-
   pushNotification(notificationEndpoint, text, dataObj) {
     let ret = new Sequence();
-    if (process.env.GNOME_ENV === 'dev') {
-      console.log('[skiping SNS] PushNotificationService:', Object.assign({ notificationEndpoint, text }, dataObj));
+    if (process.env.GNOME_ENV === "dev") {
+      console.log(
+        "[skiping SNS] PushNotificationService:",
+        Object.assign({ notificationEndpoint, text }, dataObj)
+      );
     } else if (notificationEndpoint) {
       ret = new Sequence((accept, reject) => {
         let message = { aps: Object.assign({ alert: text }, dataObj) };
 
         let params = {
           Message: this.buildMessageWith(text, message),
-          MessageStructure: 'json',
-          TargetArn: notificationEndpoint,
+          MessageStructure: "json",
+          TargetArn: notificationEndpoint
         };
-
         sns.publish(params, (err, result) => {
           if (err) {
-            console.error('Notification publication failed', notificationEndpoint, JSON.stringify(err));
+            console.error(
+              "Notification publication failed",
+              notificationEndpoint,
+              JSON.stringify(err)
+            );
             reject(err);
           } else {
-            console.info('Notification sent to ', notificationEndpoint);
+            console.info("Notification sent to ", notificationEndpoint);
             accept(result);
           }
         });
@@ -53,22 +57,28 @@ class PushNotificationService {
     let ret = new Sequence();
     if (data.creator.notificationEndpoint) {
       ret = new Sequence((accept, reject) => {
-        let text = `${commenter.firstName} ${commenter.lastName} has commented on your post`;
+        let text = `${commenter.firstName} ${
+          commenter.lastName
+        } has commented on your post`;
         let message = { aps: { alert: text, postID: data.post.postID } };
         let endpoint = data.creator.notificationEndpoint;
 
         let params = {
           Message: this.buildMessageWith(text, message),
-          MessageStructure: 'json',
-          TargetArn: endpoint,
+          MessageStructure: "json",
+          TargetArn: endpoint
         };
 
         sns.publish(params, (err, result) => {
           if (err) {
-            console.error('Notification publication failed', endpoint, JSON.stringify(err));
+            console.error(
+              "Notification publication failed",
+              endpoint,
+              JSON.stringify(err)
+            );
             reject(err);
           } else {
-            console.info('Notification sent to ', endpoint);
+            console.info("Notification sent to ", endpoint);
             accept(result);
           }
         });
@@ -78,9 +88,11 @@ class PushNotificationService {
   }
 
   register(userID, deviceToken, deviceType) {
-
     return new Sequence((accept, reject) => {
-      let PlatformApplicationArn = (deviceType || '') === 'android' ? androidApplicationArn : appleApplicationArn;
+      let PlatformApplicationArn =
+        (deviceType || "") === "android"
+          ? androidApplicationArn
+          : appleApplicationArn;
       sns.createPlatformEndpoint(
         { PlatformApplicationArn, Token: deviceToken, CustomUserData: userID },
         (err, responseData) => {
@@ -88,18 +100,23 @@ class PushNotificationService {
             let dupErrorRegexp = /Endpoint (\s+) already exists/;
             let endpointArr = dupErrorRegexp.exec(err.message);
             if (endpointArr && endpointArr.length > 0) {
-              sns.deleteEndpoint({ EndpointArn: endpointArr[0] }, (deleteErr) => {
+              sns.deleteEndpoint({ EndpointArn: endpointArr[0] }, deleteErr => {
                 if (deleteErr) {
                   reject(deleteErr);
                 } else {
                   sns.createPlatformEndpoint(
-                    { PlatformApplicationArn, Token: deviceToken, CustomUserData: userID },
+                    {
+                      PlatformApplicationArn,
+                      Token: deviceToken,
+                      CustomUserData: userID
+                    },
                     (createErr, createResponseData) => {
                       if (createErr) {
                         reject(createErr);
                       }
                       accept(createResponseData);
-                    });
+                    }
+                  );
                 }
               });
             } else {
@@ -107,7 +124,8 @@ class PushNotificationService {
             }
           }
           accept(responseData);
-        });
+        }
+      );
     }).then((accept, reject, responseData) => {
       userModel
         .updateUser(userID, { notificationEndpoint: responseData.EndpointArn })
@@ -119,28 +137,31 @@ class PushNotificationService {
   unregister(user) {
     if (user.notificationEndpoint) {
       // Best effort to remove the endpoint in AWS SNS, but there are mitigations in the register method
-      sns.deleteEndpoint({ EndpointArn: user.notificationEndpoint }, (err) => {
+      sns.deleteEndpoint({ EndpointArn: user.notificationEndpoint }, err => {
         if (err) {
           console.log(err, err.stack); // an error occurred (but we can safely ignore it)
         }
       });
-      userModel.updateUser(user.userID, { notificationEndpoint: '' }).done(() => {
-        // noop
-      }).error((err) => {
-        console.log(err, err.stack);
-      });
+      userModel
+        .updateUser(user.userID, { notificationEndpoint: "" })
+        .done(() => {
+          // noop
+        })
+        .error(err => {
+          console.log(err, err.stack);
+        });
     }
   }
 
   buildMessageWith(text, message) {
     let msg = {
-      default: text,
+      default: text
     };
     let messageString = JSON.stringify(message);
 
     msg.GCM = JSON.stringify({ data: { message: text } });
 
-    if (process.env.GNOME_ENV === 'prod') {
+    if (process.env.GNOME_ENV === "prod") {
       msg.APNS = messageString;
     } else {
       msg.APNS_SANDBOX = messageString;
--- api/src/workers/NotificationWorker.js
+++ api/src/workers/NotificationWorker.js
@@ -1,31 +1,39 @@
-'use strict';
+"use strict";
 
-const request = require('request');
-const Config = require('config/server');
-const Worker = require('core/Worker');
-const postModel = require('models/PostModel');
-const userModel = require('models/UserModel');
-const userActivityModel = require('models/UserActivityModel');
-const pushNotificationService = require('middleware/PushNotificationService');
+const request = require("request");
+const Config = require("config/server");
+const Worker = require("core/Worker");
+const postModel = require("models/PostModel");
+const userModel = require("models/UserModel");
+const userActivityModel = require("models/UserActivityModel");
+const pushNotificationService = require("middleware/PushNotificationService");
+const Nodemailer = require("nodemailer");
+var ses = require("nodemailer-ses-transport");
 
+const AWS = require("aws-sdk");
 class NotificationWorker extends Worker {
   constructor() {
-    super('notification');
-
-    this.on('N_COMMENT', this.on_COMMENT.bind(this));
-    this.on('N_REPLY', this.on_REPLY.bind(this));
-    this.on('N_REPORT', this.on_REPORT.bind(this));
+    super("notification");
+    AWS.config.update({
+      accessKeyId: Config.aws.accessKey,
+      logger: console.info,
+      region: "eu-west-1",
+      secretAccessKey: Config.aws.secretKey,
+      sslEnabled: true
+    });
+    this.on("N_COMMENT", this.on_COMMENT.bind(this));
+    this.on("N_REPLY", this.on_REPLY.bind(this));
+    this.on("N_REPORT", this.on_REPORT.bind(this));
   }
 
-
   _getActivityNode(activityID) {
-    return userActivityModel.getUserActivityByID(activityID)
+    return userActivityModel.getUserActivityByID(activityID);
   }
 
   on_REPORT(msg, id) {
     let data = msg.data;
-    if (!data.hasOwnProperty('text')) {
-      console.error('N_REPORT', 'missing text');
+    if (!data.hasOwnProperty("text")) {
+      console.error("N_REPORT", "missing text");
       return;
     }
 
@@ -33,82 +41,167 @@ class NotificationWorker extends Worker {
       let text = data.text;
       let slackToken = Config.slack.token;
       let room = Config.slack.room;
-      let url = `https://slack.com/api/chat.postMessage?token=${slackToken}&channel=${room}&text=${text}&username=NEW%20REPORT&icon_emoji=%3Awarning%3A&pretty=1`
+      let url = `https://slack.com/api/chat.postMessage?token=${slackToken}&channel=${room}&text=${text}&username=NEW%20REPORT&icon_emoji=%3Awarning%3A&pretty=1`;
       request.get(url); //TODO: create middleware to do this
     } else {
-      console.warn('Missing SLACK_TOKEN. skipping message.')
+      console.warn("Missing SLACK_TOKEN. skipping message.");
     }
   }
 
-
   on_COMMENT(msg, id) {
     let data = msg.data;
-    if (!data.hasOwnProperty('activityID')) {
-      console.error('on_COMMENT', 'missing activityID');
+    if (!data.hasOwnProperty("activityID")) {
+      console.error("on_COMMENT", "missing activityID");
       return;
     }
-    let activityID = data.activityID
+    let activityID = data.activityID;
 
-    this._getActivityNode(activityID).then((accept, reject, Activity) => {
-      userModel.getBlockStatus(Activity.userID, Activity.actor.userID).done((status) => {
-        if (status === null) {
-          userActivityModel.getUserActivityCount(Activity.userID).done((counts) =>{
-            accept(Activity, counts.unSeen);
-          }).error(() => {
-            accept(Activity, 0);
-          });
-        } else {
-          reject(`USER ${Activity.userID} and ${Activity.actor.userID} are blocking each other`)
-        }
-      }).error(reject);
-    }).done((Activity, count) => {
+    this._getActivityNode(activityID)
+      .then((accept, reject, Activity) => {
+        userModel
+          .getBlockStatus(Activity.userID, Activity.actor.userID)
+          .done(status => {
+            if (status === null) {
+              userActivityModel
+                .getUserActivityCount(Activity.userID)
+                .done(counts => {
+                  accept(Activity, counts.unSeen);
+                })
+                .error(() => {
+                  accept(Activity, 0);
+                });
+            } else {
+              reject(
+                `USER ${Activity.userID} and ${
+                  Activity.actor.userID
+                } are blocking each other`
+              );
+            }
+          })
+          .error(reject);
+      })
+      .done((Activity, count) => {
+        let endpoint = Activity.person.email || null;
+        let text = [
+          `<p><u><i>${Activity.actor.username}</i></u>`,
+          `${Activity.text}</p>`,
+          `<p> " <i>${Activity.target.Comment.content}</i>" `,
+          `&mdash; get in touch via ${Activity.target.Comment.author.email}`
+        ].join(" ");
 
-      let endpoint = Activity.person.notificationEndpoint || null;
-      let text = [Activity.actor.username, Activity.text].join(' ');
-
-      pushNotificationService.pushNotification(endpoint, text, {
-        activityID: Activity.activityID,
-        type: Activity.type,
-        postID: Activity.target.Post.postID,
-        Badge: count
-      }).error(console.error.bind(console));
-    }).error(console.error.bind(console));
+        this.sendCommentNotificationEmail(endpoint, text);
+        //commented out as we are not using pushnotification
+        // pushNotificationService
+        //   .pushNotification(endpoint, text, {
+        //     activityID: Activity.activityID,
+        //     type: Activity.type,
+        //     postID: Activity.target.Post.postID,
+        //     Badge: count
+        //   })
+        //   .error(console.error.bind(console));
+      })
+      .error(console.error.bind(console));
   }
 
   on_REPLY(msg, id) {
     let data = msg.data;
-    if (!data.hasOwnProperty('activityID')) {
-      console.error('on_COMMENT', 'missing activityID');
+    if (!data.hasOwnProperty("activityID")) {
+      console.error("on_COMMENT", "missing activityID");
       return;
     }
-    let activityID = data.activityID
+    let activityID = data.activityID;
 
-    this._getActivityNode(activityID).then((accept, reject, Activity) => {
-      userModel.getBlockStatus(Activity.userID, Activity.actor.userID).done((status) => {
-        if (status === null) {
-          userActivityModel.getUserActivityCount(Activity.userID).done((counts) =>{
-            accept(Activity, counts.unSeen);
-          }).error(() => {
-            accept(Activity, 0);
-          });
-        } else {
-          reject(`USER ${Activity.userID} and ${Activity.actor.userID} are blocking each other`)
-        }
-      }).error(reject);
-    }).done((Activity, count) => {
-      let endpoint = Activity.person.notificationEndpoint || null;
-      let text = [Activity.actor.username, Activity.text].join(' ');
+    this._getActivityNode(activityID)
+      .then((accept, reject, Activity) => {
+        userModel
+          .getBlockStatus(Activity.userID, Activity.actor.userID)
+          .done(status => {
+            if (status === null) {
+              userActivityModel
+                .getUserActivityCount(Activity.userID)
+                .done(counts => {
+                  accept(Activity, counts.unSeen);
+                })
+                .error(() => {
+                  accept(Activity, 0);
+                });
+            } else {
+              reject(
+                `USER ${Activity.userID} and ${
+                  Activity.actor.userID
+                } are blocking each other`
+              );
+            }
+          })
+          .error(reject);
+      })
+      .done((Activity, count) => {
+        let endpoint = Activity.person.notificationEndpoint || null;
+        let text = [Activity.actor.username, Activity.text].join(" ");
 
-      pushNotificationService.pushNotification(endpoint, text, {
-        activityID: Activity.activityID,
-        type: Activity.type,
-        postID: Activity.target.Post.postID,
-        Badge: count
-      }).error(console.error.bind(console));
-    }).error(console.warn.bind(console));
+        pushNotificationService
+          .pushNotification(endpoint, text, {
+            activityID: Activity.activityID,
+            type: Activity.type,
+            postID: Activity.target.Post.postID,
+            Badge: count
+          })
+          .error(console.error.bind(console));
+      })
+      .error(console.warn.bind(console));
   }
 
-}
+  sendCommentNotificationEmail(notificationEndpoint, body) {
+    //using nodemailer-ses-transport
+    // var transporter = Nodemailer.createTransport(
+    //   ses({
+    //     accessKeyId: "AKIAJDSXMCSLYOBYRQ4A",
+    //     logger: console.info,
+    //     region: "eu-west-1",
+    //     secretAccessKey: "WirnJSSQbgQI/zj8rcDszqaeTXng+z7EOc3+8C7+",
+    //     sslEnabled: true
+    //   })
+    // );
 
+    // using sms directly
+    var params = {
+      Destination: {
+        ToAddresses: [notificationEndpoint]
+      },
+      Message: {
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: body
+          },
+          Text: {
+            //The email body for recipients with non-HTML email clients.
+            Charset: "UTF-8",
+            Data: body
+          }
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: "humankind: a comment is made on your post"
+        }
+      },
+      Source: Config.smtp.from
+    };
+
+    // Create the promise and SES service object
+    var sendPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+      .sendEmail(params)
+      .promise();
+
+    // Handle promise's fulfilled/rejected states
+    sendPromise
+      .then(function(data) {
+        console.log(data.MessageId);
+      })
+      .catch(function(err) {
+        console.error(err, err.stack);
+      });
+  }
+}
 
 module.exports = NotificationWorker;
