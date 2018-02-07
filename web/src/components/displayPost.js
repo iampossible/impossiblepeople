2d5523b074236a25339701033f29cfaffe6b75d8
KiyaG <hamilee.kiya@gmail.com>
2018-02-07 15:11:25 +0000

    included the DisplayComment component and the display comment icon and showing the number of the comments

--- web/src/components/displayPost.js
+++ web/src/components/displayPost.js
@@ -1,11 +1,30 @@
 import React, { Component, Fragment } from "react";
 import { Row, Col, Tooltip } from "reactstrap";
-import * as moment from "moment";
+import DisplayComment from "./displayComment";
 
 class DisplayPost extends Component {
   state = {
     updateTooltipOpen: false,
-    deleteTooltipOpen: false
+    deleteTooltipOpen: false,
+    loadComment: "",
+    commentButtonText: "fa fa-angle-double-down"
+  };
+
+  toggleComment = e => {
+    let commentsDisplayed = this.state.commentsDisplayed;
+    if (!commentsDisplayed) {
+      this.setState({
+        loadComment: !this.state.loadComment,
+        commentButtonText: "fa fa-angle-double-up",
+        commentsDisplayed: true
+      });
+    } else {
+      this.setState({
+        loadComment: !this.state.loadComment,
+        commentButtonText: "fa fa-angle-double-down",
+        commentsDisplayed: false
+      });
+    }
   };
 
   toggleUpdatePost = () => {
@@ -24,155 +43,173 @@ class DisplayPost extends Component {
     const postData = this.props.postData;
     const user = this.props.user;
     return (
-      <Row xs={12}>
-        <Col sm={3} xs={12}>
-          <Row className="feedPhoto">
-            <Col sm={1} />
-            <Col sm={10}>
-              <img
-                className="img-fluid feedPhoto"
-                src={postData.author.imageSource}
-                alt="profile"
-              />
-              <blockquote className="blockquote">
-                <footer className="blockquote-footer">
-                  <span className="feedAuthor">
-                    {//temporary as we have data in the db that doesn't have organisationName
-                    postData.author.organisationName
-                      ? postData.author.organisationName
-                      : postData.author.username}
-                  </span>&nbsp;
-                  <p className="feedPostType">
-                    <i
-                      className="fa fa-sm fa-chevron-right"
-                      aria-hidden="true"
-                    />
-                    &nbsp;
-                    {postData.postType}
-                  </p>
-                </footer>
-              </blockquote>
-            </Col>
-            <Col sm={1} />
-          </Row>
-        </Col>
-        <Col className="feedBody" sm={9} xs={12}>
+      <Fragment>
+        {postData.author.userID === user.userID || user.admin ? (
           <Row>
-            <Col xs={12}>
-              {postData.author.userID === user.userID ? (
-                <Row xs={12}>
-                  <Col xs={10} />
-                  <Col xs={2} className="updateDeletePostButtonsContainer">
-                    <a
-                      id="ToolTipUpdateIcon"
-                      className="updatePostIcon"
-                      href="#post"
-                      onClick={() =>
-                        this.props.handlePostUpdate(postData.postID)
-                      }>
-                      <i className="material-icons">edit</i>
-                    </a>
-                    <Tooltip
-                      placement="left"
-                      isOpen={this.state.updateTooltipOpen}
-                      target="ToolTipUpdateIcon"
-                      toggle={this.toggleUpdatePost}>
-                      Update Post
-                    </Tooltip>
-                    <a
-                      id="TooltipDeleteIcon"
-                      className="deletePostIcon"
-                      onClick={() =>
-                        this.props.handlePostDelete(postData.postID)
-                      }>
-                      <i className="material-icons">clear</i>
-                    </a>
-                    <Tooltip
-                      placement="right"
-                      isOpen={this.state.deleteTooltipOpen}
-                      target="TooltipDeleteIcon"
-                      toggle={this.toggleDeletePost}>
-                      Delete Post
-                    </Tooltip>
-                  </Col>
-                </Row>
-              ) : null}
+            <Col sm={10} />
+            <Col sm={2} className="updateDeletePostButtonsContainer">
+              <a
+                id="ToolTipUpdateIcon"
+                className="updatePostIcon"
+                href="#post"
+                onClick={() => this.props.handlePostUpdate(postData.postID)}>
+                <i className="material-icons">edit</i>
+              </a>
+              <Tooltip
+                placement="left"
+                isOpen={this.state.updateTooltipOpen}
+                target="ToolTipUpdateIcon"
+                toggle={this.toggleUpdatePost}>
+                Update Post
+              </Tooltip>
+              <a
+                id="TooltipDeleteIcon"
+                className="deletePostIcon"
+                onClick={() => this.props.handlePostDelete(postData.postID)}>
+                <i className="material-icons">clear</i>
+              </a>
+              <Tooltip
+                placement="right"
+                isOpen={this.state.deleteTooltipOpen}
+                target="TooltipDeleteIcon"
+                toggle={this.toggleDeletePost}>
+                Delete Post
+              </Tooltip>
             </Col>
           </Row>
-          {postData.imageSource ? (
-            <Row>
-              <Col sm={10} className="postImage">
-                <img src={postData.imageSource} alt={postData.postID} />
-              </Col>
-              <Col sm={2} />
-            </Row>
+        ) : null}
+
+        {postData.imageSource ? (
+          <Row>
+            <Col sm={12} className="postImage">
+              <img src={postData.imageSource} alt={postData.postID} />
+            </Col>
+          </Row>
+        ) : (
+          ""
+        )}
+        <Row>
+          <Col sm={10}>
+            <p className="feedTags">
+              <i className="fa fa-sm fa-tag" aria-hidden="true" />
+              &nbsp;&nbsp;&nbsp;
+              {/* if the post has more than one tag/interest */}
+              <span>
+                {postData.interests.map(interest => interest.name).join(" / ")}
+              </span>
+            </p>
+          </Col>
+          <Col sm={2}>
+            <p className="feedCreatedAt">
+              {/* {moment(postData.createdAt).format("MMM Do, YYYY")}&nbsp; */}
+              <i
+                className="fa fa-clock-o"
+                aria-hidden="true"
+              />&nbsp;&nbsp;{postData.createdAtSince.toUpperCase() + " ago"}
+            </p>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col sm={12}>
+            <p className="feedPostType">
+              {postData.postType}&nbsp;&nbsp;<i
+                className="fa fa-ellipsis-v"
+                aria-hidden="true"
+              />
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={1} />
+          <Col sm={10}>
+            <p className="feedContent">
+              <i className="fa fa-quote-left" aria-hidden="true" />&nbsp;&nbsp;{
+                postData.content
+              }&nbsp;&nbsp;<i
+                className="fa fa-quote-right"
+                aria-hidden="true"
+              />
+            </p>
+          </Col>
+          <Col sm={1} />
+        </Row>
+        <Row>
+          <Col sm={1} />
+          <Col sm={1} className="feedPhoto">
+            <img
+              className="img-fluid"
+              src={postData.author.imageSource}
+              alt="profile"
+            />
+          </Col>
+          <Col sm={4} className="feedAuthor">
+            <p>
+              {//temporary as we have data in the db that doesn't have organisationName
+              postData.author.organisationName
+                ? postData.author.organisationName
+                : postData.author.username}
+            </p>
+          </Col>
+        </Row>
+        <Row className="separatorHr">
+          <Col sm={1} />
+          <Col sm={10}>
+            <hr />
+          </Col>
+          <Col sm={1} />
+        </Row>
+        <Row>
+          <Col sm={1} />
+          <Col sm={4} className="feedLocationIcon">
+            <p className="feedLocation">
+              <i className="fa fa-map-marker" aria-hidden="true" />
+              &nbsp;&nbsp;&nbsp;
+              {postData.location}
+            </p>
+          </Col>
+          <Col sm={3} className="commentIcon">
+            <Fragment>
+              <i className="fa fa-comments" aria-hidden="true" />&nbsp;
+              {postData.commentCount > 0 ? (
+                <Fragment>
+                  {postData.commentCount}
+                  &nbsp; &nbsp;<i
+                    onClick={this.toggleComment}
+                    className={this.state.commentButtonText}
+                    aria-hidden="true"
+                  />
+                </Fragment>
+              ) : (
+                ""
+              )}
+            </Fragment>
+          </Col>
+          {postData.url ? (
+            <Col sm={3} className="feedUrl">
+              <span>
+                <i className="fa fas fa-link" />
+              </span>&nbsp;&nbsp;&nbsp;&nbsp;
+              <a className="feedCreatedAt" href={postData.url}>
+                Read More ...
+              </a>
+            </Col>
           ) : (
             ""
           )}
+        </Row>
+        {this.state.loadComment ? (
           <Row>
-            <Col sm={11}>
-              <blockquote className="blockquote">
-                <p className="feedContent">{postData.content}</p>
-                <footer className="blockquote-footer">
-                  <cite title="Source Title">
-                    <Row>
-                      <Col xs={1} className="feedTagsIcon">
-                        <span>
-                          <i className="fa fa-sm fa-tag" aria-hidden="true" />
-                        </span>
-                      </Col>
-                      <Col xs={11}>
-                        <p className="feedTags">
-                          {/* if the post has more than one tag/interest */}
-                          <span>
-                            {postData.interests
-                              .map(interest => interest.name)
-                              .join(" / ")}
-                          </span>
-                        </p>
-                      </Col>
-                      <Col xs={1} className="feedLocationIcon">
-                        <span>
-                          <i className="fa fa-map-marker" aria-hidden="true" />
-                        </span>
-                      </Col>
-                      <Col xs={11}>
-                        <p className="feedLocation">{postData.location}</p>
-                      </Col>
-                      <Col xs={1} className="feedCreatedAtIcon">
-                        <span>
-                          <i className="fa fa-calendar" />
-                        </span>
-                      </Col>
-                      <Col xs={11}>
-                        <p className="feedCreatedAt">
-                          {moment(postData.createdAt).format("MMM Do, YYYY")}&nbsp;
-                        </p>
-                      </Col>
-                      {postData.url ? (
-                        <Fragment>
-                          <Col xs={1} className="feedCreatedAtIcon">
-                            <span>
-                              <i className="fa fas fa-link" />
-                            </span>
-                          </Col>
-                          <Col xs={11}>
-                            <a className="feedCreatedAt" href={postData.url}>
-                              Read More ...
-                            </a>
-                          </Col>
-                        </Fragment>
-                      ) : (
-                        ""
-                      )}
-                    </Row>
-                  </cite>
-                </footer>
-              </blockquote>
+            <Col sm={1} />
+            <Col sm={10}>
+              <DisplayComment comments={postData.comments} />
             </Col>
+            <Col sm={1} />
           </Row>
-        </Col>
-      </Row>
+        ) : (
+          ""
+        )}
+      </Fragment>
     );
   }
 }
