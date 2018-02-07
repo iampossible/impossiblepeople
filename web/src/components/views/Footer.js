18c957508af4736a1d567e0f313cd7bb8a58db13
KiyaG <hamilee.kiya@gmail.com>
2018-02-07 15:25:05 +0000

    replaced the Container element and with the facebook's Fragment

--- web/src/components/views/Footer.js
+++ web/src/components/views/Footer.js
@@ -1,32 +1,30 @@
-import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
+import React, { Component, Fragment } from "react";
+import { Col } from "reactstrap";
 
 export default class Footer extends Component {
   render() {
     return (
-      <Container id="footer">
-        <Row>
-          <Col sm={4} className="d-none d-sm-block" />
-          <Col xs={4} sm={2} id="termsOfUse">
-            <p>
-              <a href="#">Terms of user</a>
-            </p>
-          </Col>
-          <Col xs={5} sm={2} id="privacyPlicy">
-            <p>
-              <a href="#">Privacy Policy</a>
-            </p>
-          </Col>
-          <Col sm={1} className="d-none d-sm-block" />
-          <Col xs={3} sm={3} id="footerCopyright">
-            <p>
-              <span className="d-none d-sm-inline">We Are One</span>&nbsp;
-              <i className="fa fa-copyright" aria-hidden="true" />&nbsp;
-              {new Date().getFullYear()}
-            </p>
-          </Col>
-        </Row>
-      </Container>
+      <Fragment>
+        <Col sm={4} className="d-none d-sm-block" />
+        <Col xs={4} sm={2} id="termsOfUse">
+          <p>
+            <a href="#">Terms of user</a>
+          </p>
+        </Col>
+        <Col xs={5} sm={2} id="privacyPlicy">
+          <p>
+            <a href="#">Privacy Policy</a>
+          </p>
+        </Col>
+        <Col sm={1} className="d-none d-sm-block" />
+        <Col xs={3} sm={3} id="footerCopyright">
+          <p>
+            <span className="d-none d-sm-inline">Humankind</span>&nbsp;
+            <i className="fa fa-copyright" aria-hidden="true" />&nbsp;
+            {new Date().getFullYear()}
+          </p>
+        </Col>
+      </Fragment>
     );
   }
 }
