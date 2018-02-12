ba13d2cca32030ccefc0848e54c7747db685d1a2
KiyaG <hamilee.kiya@gmail.com>
2018-02-12 13:26:46 +0000

    fixed the logOut and signIn buttons position

--- web/src/components/views/Header.js
+++ web/src/components/views/Header.js
@@ -31,84 +31,107 @@ class Header extends Component {
     const headerImage =
       "https://humankind-assets.s3.eu-west-1.amazonaws.com/post/GompTy5bPN3G9";
     return (
-      <header>
-        <Row>
-          {location.pathname === "/" ? <Col sm={4} /> : null}
-          {location.pathname !== "/" ? (
-            <Col sm={2} id="headerProfileLink">
-              <span
-                className={location.pathname === "/profile" ? "activePage" : ""}
-                onClick={() => {
-                  this.props.history.push({
-                    pathname: "/profile"
-                  });
-                }}>
-                <img src={user.imageSource} id="myProfileLinkImage" />{" "}
-                &nbsp;&nbsp;&nbsp;&nbsp;My Profile
-              </span>
-            </Col>
-          ) : null}
+      <Row>
+        {location.pathname === "/" ||
+        location.pathname === "/userAgreement" ||
+        location.pathname === "/privacyPolicy" ||
+        location.pathname === "/faq" ||
+        location.pathname === "/feedback" ? (
+          <Col sm={4} />
+        ) : null}
 
-          {location.pathname !== "/" ? (
-            <Col sm={2} id="headerFeedLink">
-              <span
-                className={location.pathname === "/feed" ? "activePage" : ""}
-                onClick={() => {
-                  this.props.history.push({
-                    pathname: "/feed"
-                  });
-                }}>
-                <i className="fa fa-file-text" aria-hidden="true" />
-                &nbsp;&nbsp;&nbsp;&nbsp;My Feed
-              </span>
-            </Col>
-          ) : null}
-
-          <Col sm={4} id="headerImage">
-            <img src={headerImage} alt="header" />
-          </Col>
-
-          {user && user.admin ? (
-            <Col sm={2} id="headerApproveOrgsLink">
-              <span
-                className={location.pathname === "/admin" ? "activePage" : ""}
-                onClick={() => {
-                  this.props.history.push({
-                    pathname: "/admin"
-                  });
-                }}>
-                Manage&nbsp;&nbsp;<i
-                  className="fa fa-gears"
-                  aria-hidden="true"
-                />
-              </span>
-            </Col>
-          ) : null}
-
-          {location.pathname !== "/" ? (
-            <Col sm={1} id="headerLogoutLink">
-              <span onClick={this.handlelogout}>
-                Logout &nbsp;&nbsp;<i
-                  className="fa fa-sign-out"
-                  aria-hidden="true"
-                />
-              </span>
-            </Col>
-          ) : (
-            <Fragment>
-              <Col sm={1} />
-              <Col sm={1} id="headerSignInLink">
+        {location.pathname !== "/userAgreement" &&
+        location.pathname !== "/privacyPolicy" &&
+        location.pathname !== "/faq" &&
+        location.pathname !== "/feedback" ? (
+          <Fragment>
+            {location.pathname !== "/" ? (
+              <Col sm={2} id="headerProfileLink">
                 <span
-                  name="dispalyLoginForm"
-                  onClick={this.props.toggleDisplayForm}>
-                  Sign in &nbsp;&nbsp;
-                  <i className="fa fa-lock" aria-hidden="true" />
+                  className={
+                    location.pathname === "/profile" ? "activePage" : ""
+                  }
+                  onClick={() => {
+                    this.props.history.push({
+                      pathname: "/profile"
+                    });
+                  }}>
+                  <img src={user.imageSource} id="myProfileLinkImage" />{" "}
+                  &nbsp;&nbsp;&nbsp;&nbsp;My Profile
                 </span>
               </Col>
-            </Fragment>
-          )}
-        </Row>
-      </header>
+            ) : null}
+            {location.pathname !== "/" ? (
+              <Col sm={2} id="headerFeedLink">
+                <span
+                  className={location.pathname === "/feed" ? "activePage" : ""}
+                  onClick={() => {
+                    this.props.history.push({
+                      pathname: "/feed"
+                    });
+                  }}>
+                  <i className="fa fa-file-text" aria-hidden="true" />
+                  &nbsp;&nbsp;&nbsp;&nbsp;My Feed
+                </span>
+              </Col>
+            ) : null}
+          </Fragment>
+        ) : null}
+        <Col sm={4} id="headerImage">
+          <img src={headerImage} alt="header" />
+        </Col>
+        {user &&
+        user.admin &&
+        location.pathname !== "/userAgreement" &&
+        location.pathname !== "/privacyPolicy" &&
+        location.pathname !== "/faq" &&
+        location.pathname !== "/feedback" ? (
+          <Col sm={2} id="headerApproveOrgsLink">
+            <span
+              className={location.pathname === "/admin" ? "activePage" : ""}
+              onClick={() => {
+                this.props.history.push({
+                  pathname: "/admin"
+                });
+              }}>
+              Manage&nbsp;&nbsp;<i className="fa fa-gears" aria-hidden="true" />
+            </span>
+          </Col>
+        ) : (
+          <Col sm={1} />
+        )}
+        {location.pathname !== "/" &&
+        location.pathname !== "/userAgreement" &&
+        location.pathname !== "/privacyPolicy" &&
+        location.pathname !== "/faq" &&
+        location.pathname !== "/feedback" ? (
+          <Col sm={2} id="headerLogoutLink">
+            <span onClick={this.handlelogout}>
+              Logout &nbsp;&nbsp;<i
+                className="fa fa-sign-out"
+                aria-hidden="true"
+              />
+            </span>
+          </Col>
+        ) : null}
+        {location.pathname !== "/userAgreement" &&
+        location.pathname !== "/privacyPolicy" &&
+        location.pathname !== "/faq" &&
+        location.pathname !== "/feedback" &&
+        location.pathname === "/" ? (
+          <Fragment>
+            <Col sm={2} />
+            <Col sm={1} id="headerSignInLink">
+              <span
+                name="dispalyLoginForm"
+                onClick={this.props.toggleDisplayForm}>
+                Sign in &nbsp;&nbsp;
+                <i className="fa fa-lock" aria-hidden="true" />
+              </span>
+            </Col>
+          </Fragment>
+        ) : null}
+      </Row>
     );
   }
 }
