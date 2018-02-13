9e9a3ba210899415afc75bfc18744a2c0476931c
KiyaG <hamilee.kiya@gmail.com>
2018-02-13 01:38:48 +0000

    modified the content of the UserAgreement component

--- web/src/components/views/UserAgreement.js
+++ web/src/components/views/UserAgreement.js
@@ -4,18 +4,450 @@ import { Row, Col } from "reactstrap";
 export default class UserAgreement extends Component {
   render() {
     return (
-      <Row>
-        <Col sm={2} />
-        <Col sm={10}>
+      <Row className="footerLinks">
+        <Col sm={12}>
           <Row>
-            <Col sm={2} />
+            <Col sm={1} />
             <Col sm={10}>
-              <h1>User Agreement</h1>
+              <h4>
+                <u>Humankind</u>&nbsp;&mdash;&nbsp;Terms and Conditions
+              </h4>
             </Col>
-            <Col sm={2} />
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <h6>1.&nbsp;DEFINITIONS</h6>
+              <p>
+                In these terms and conditions the following words have the
+                following meanings unless the context requires otherwise:
+              </p>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p>
+                <strong>&quot;App&quot;</strong> means Our HumanKind mobile
+                application available at www.humanKIndhub.org (.com and .co.uk )
+                or such other URL that we may use to provide the Service from
+                time to time), the Apple App Store and the Google Play Store;
+              </p>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <strong>&quot;Content&quot;</strong> means all of the content (
+              words and hyperlinks) that are posted by a User or HumanKind on
+              the App;
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <strong>&quot;Responsibility&quot;</strong> means the liability of
+              any nature including but not limited to any and all damages,
+              claims, proceedings, actions, awards, expenses, costs and any
+              other losses and/or liabilities;
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <strong>&quot;“Service&quot;</strong> means the service provided
+              by us to you including allowing you to; access to the App,  browse
+              and use the App, contact and be contacted by other members through
+              the App and post Content on the App;
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <strong>&quot;HumanKind&quot;</strong> is a registered product
+              licensed to Impossible Labs and weareone:collective
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <strong>&quot;User&quot;</strong> means any individual, or
+              representative of an organisation that registers on the HumanKind
+              platform.
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <h6>2.&nbsp;Pre&#45;registration</h6>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p>
+                <strong>Your Account</strong>
+              </p>
+              <p>
+                To use the App you must <strong>NOT</strong>:
+              </p>
+              <ul>
+                <li>be under eighteen (18) years of age;</li>
+                <li>
+                  have been convicted of any offence relating to violence or any
+                  offence under the Protection from Harassment Act 1997 (or
+                  under any statutory enactment replacing and/or amending such
+                  Act);
+                </li>
+                <li>
+                  have been subject to any injunction or any order to pay
+                  damages under the Protection from Harassment Act 1997 (or
+                  under any statutory enactment replacing and/or amending such
+                  Act);
+                </li>
+                <li>
+                  be registered as a sexual offender under the Sexual Offences
+                  Act 2003;
+                </li>
+                <li>let anyone else set up or post content on your profile;</li>
+                <li>use the App for any illegal activity;</li>
+                <li>
+                  post content which is inaccurate, untrue, incomplete or
+                  misleading; or
+                </li>
+                <li>
+                  fail to update information when it becomes inaccurate, untrue,
+                  incomplete or misleading.
+                </li>
+                <li>
+                  If you fall under any of the above bullet points either before
+                  or during your HumanKind membership, we may immediately block,
+                  suspend or terminate your account.
+                </li>
+              </ul>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <h6>3.&nbsp;Your profile</h6>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p style={{ paddingLeft: "3em" }}>
+                This is a Beta test version of the HumanKind Platform. Features
+                and terms will change and vary as the app develops.
+              </p>
+              <p style={{ paddingLeft: "3em" }}>
+                When creating a profile to test the product you are
+                pre-registering for the Final product and your details will be
+                migrated to the live platform at the time of launch. You will
+                receive an email notifying you of this and if you wish to delete
+                your profile or alter your settings you can so by emailing us on
+                support@humankindhub.org
+              </p>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p>
+                When setting up your profile, you should think carefully about
+                what Content you post and try to represent yourself fairly.
+              </p>
+              <p>
+                You understand and agree that we will make any Content you post
+                to your profile available for everyone to see.
+              </p>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <h6>4.&nbsp;Responsibilities</h6>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p>You have responsibility to:</p>
+              <ul>
+                <li>
+                  read and comply with these terms and conditions and our
+                  policies on privacy and safety
+                </li>
+                <li>
+                  keep your password private and do your best to protect against
+                  unauthorised access to the device you use to access the App;
+                </li>
+                <li>
+                  Immediately change your password and let us know if the device
+                  you use to access the App is lost, stolen, or accessed without
+                  your permission, or if your username or password are used
+                  without your permission. You can let us know by emailing
+                  hello@humankindhub.org
+                </li>
+                <li>
+                  be honest and truthful when completing your profile,
+                  particularly with respect to your identity, experience or
+                  capabilities
+                </li>
+                <li>
+                  let us know immediately if you become aware of any
+                  inappropriate behaviour on the Service; and
+                </li>
+              </ul>
+              <p>You must not:</p>
+              <ul>
+                <li>
+                  post any illegal content or content that would contravene
+                  someone else’s rights (including privacy rights), or post
+                  copyrighted material, without the copyright holder’s
+                  consent).  If you are not sure, check with us first by
+                  emailing: hello@humankindhub.org
+                </li>
+                <li>
+                  use the app for commercial trading; attempt to sell products
+                  and/or commercial services; or charge any fee at any point
+                  when using the platform.
+                </li>
+                <li>share or ask other members to share bank details;</li>
+                <li>
+                  share or ask other members to share personal information,
+                  except where necessary to use the Services and as permitted
+                  under these terms and conditions;
+                </li>
+                <li>misuse the App or any Content on the App;</li>
+                <li>
+                  include the Service within another external website without
+                  our written permission;
+                </li>
+                <li>
+                  set up a profile for someone else without their permission;
+                </li>
+                <li>attempt to hack the App or anyone else’s account;</li>
+                <li>copy any Content from the App without permission;</li>
+                <li>
+                  post any junk mail, spam, chain letters, pyramid schemes or
+                  any similar or fraudulent schemes;
+                </li>
+                <li>
+                  give, sell or let anyone else use your account, password or
+                  profile;
+                </li>
+                <li>use, access, or publish the App without consent;</li>
+                <li>
+                  post any sexually explicit, vulgar, indecent, racist, abusive,
+                  invasive of another&#39;s privacy, offensive, harmful,
+                  threatening, harassing, stalking, inflammatory, sexually
+                  suggestive, defamatory and/or obscene content;
+                </li>
+                <li>
+                  post any Content which is capable of offending others’
+                  political and/or religious beliefs; or
+                </li>
+                <li>
+                  encourage or promote any activity that violates these Terms
+                  &amp; Conditions.
+                </li>
+              </ul>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <h6>5.&nbsp;The Service</h6>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p>
+                This is a non-profit service and so our financial circumstances
+                may vary, limiting our ability to optimise the Service.
+              </p>
+              <p>
+                We do not actively monitor the Content that members post and so
+                can’t guarantee that the Service is accurate or reliable. You
+                use the Service at your own risk and should follow our Safety
+                guidelines found in the app.
+              </p>
+              <p>
+                We cannot guarantee that the Service will always be available
+                and uninterrupted but we will try to rectify serious faults as
+                soon as is practical. We may make changes (so long as they do
+                not seriously affect the quality of the Service) and may suspend
+                the Service at any point for repair, maintenance or other
+                technical reasons.
+              </p>
+              <p>
+                We may delete messages if your account is suspended or
+                terminated. If you wish to save your messages you should take
+                steps to back up and store them yourself.
+              </p>
+              <p>
+                We may copy and use any Content posted to your profile.  We may
+                review, amend and delete your profile if necessary. All personal
+                data contained in the information you provide when setting up
+                your account and the Content you post is subject to our
+              </p>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <h6>6.&nbsp;Cancellation, Termination and Suspension</h6>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p>
+                You may cancel your account at any time. To do so contact us at
+                <strong>hello@humankind.org</strong>
+              </p>
+              <p>
+                We may suspend or terminate your account immediately if you
+                breach these terms and conditions; or post anything that could
+                be damaging to HumanKind or other members; or if you fail to
+                respond when we ask for information to determine the accuracy of
+                any Content you post.
+              </p>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <h6>7.&nbsp;Limitation of our Responsibility</h6>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p>We will have no Responsibility:</p>
+              <ul>
+                <li>
+                  for any defect or delay in the Service caused or contributed
+                  to by you, any other member or factors outside of our control;
+                </li>
+                <li>
+                  for your reliance on any Content posted by other members;
+                </li>
+                <li>
+                  to you arising out of any User’s misuse of Content posted by
+                  you or other members;
+                </li>
+                <li>
+                  HumanKind makes no representation or warranties as to the
+                  background or conduct of its Users;
+                </li>
+                <li>
+                  to you if that Responsibility is covered by any policy of
+                  insurance;
+                </li>
+                <li>
+                  to you for any:
+                  <ul>
+                    <li>consequential, special or indirect losses;</li>
+                    <li>
+                      economic and/or other similar losses (including loss of
+                      revenues, profits, contracts, business or anticipated
+                      savings); or
+                    </li>
+                    <li>damage to or loss of goodwill, reputation or data;</li>
+                  </ul>
+                </li>
+                <li>
+                  to you for the presence of any offensive Content on the
+                  Service.
+                </li>
+              </ul>
+            </Col>
+            <Col sm={1} />
+          </Row>
+
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p>
+                You will give us reasonable opportunity to remedy any breach of
+                these terms and conditions before we have Responsibility. You
+                should mitigate any loss, damage, costs or expenses that you may
+                suffer. We do not limit our Responsibility for death, personal
+                injury or fraud or any other responsibility which we are not
+                permitted to limit by law. This section applies to our
+                directors, officers, employees, subcontractors, agents and
+                affiliated companies as well as to us.
+              </p>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <h6>8.&nbsp;General terms</h6>
+            </Col>
+            <Col sm={1} />
+          </Row>
+          <Row>
+            <Col sm={1} />
+            <Col sm={10}>
+              <p>
+                We may amend these terms and conditions at any time. If we do,
+                the amended version will be posted on the terms and conditions
+                section of the App so you should check this section often.
+              </p>
+              <p>
+                These terms and conditions along with the  ****** constitute the
+                entire agreement between you and us.
+              </p>
+              <p>
+                If a competent authority finds any part of these terms and
+                conditions to be invalid or unenforceable, the validity of the
+                other parts and the remainder of the affected part will not be
+                affected.
+              </p>
+              <p>
+                We may assign our rights and duties under these terms and
+                conditions; you may not do so without our permission.
+              </p>
+              <p>
+                We will contact you by email using the most recent email you
+                provided and you may contact us by email using <strong>
+                  hello@humankind.org
+                </strong>
+              </p>
+              <p>
+                These terms and conditions are governed by and interpreted in
+                accordance with English law and the parties agree to submit to
+                the exclusive jurisdiction of the English courts.
+              </p>
+            </Col>
+            <Col sm={1} />
           </Row>
         </Col>
-        <Col sm={2} />
       </Row>
     );
   }
