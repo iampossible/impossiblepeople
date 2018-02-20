c9ca1443ab72da67df6346bb5ba3d115329a5368
KiyaG <hamilee.kiya@gmail.com>
2018-02-20 16:45:10 +0000

    included error message for faliure to load user profile

--- web/src/components/views/Profile.js
+++ web/src/components/views/Profile.js
@@ -1,22 +1,16 @@
 import React, { Component, Fragment } from "react";
-import { Row, Col } from "reactstrap";
+import { Row, Col, Alert } from "reactstrap";
 import { handleErrors } from "../../utillity/helpers";
+import { RingLoader } from "react-spinners";
 
 export default class Profile extends Component {
   state = {
-    user: {},
-    loading: false
+    user: null,
+    profileLoadError: null
   };
 
   componentWillMount() {
-    this.setState(
-      {
-        loading: true
-      },
-      () => {
-        this.getUser(this.props.userID);
-      }
-    );
+    this.getUser(this.props.userID);
   }
   getUser = userID => {
     const TIME_OUT_SECOND = 6000;
@@ -31,21 +25,19 @@ export default class Profile extends Component {
       .then(response => response.json())
       .then(response => {
         this.setState({
-          user: response,
-          loading: false
+          user: response
         });
       })
       .catch(err => {
         this.setState(
           {
-            loading: false,
             profileLoadError: `Oopa!, something happend with getting the user profile. Please, try again later`
           },
           () => {
             //clear the error message
             setTimeout(() => {
               this.setState({
-                imageLoadError: null
+                profileLoadError: null
               });
             }, TIME_OUT_SECOND);
           }
@@ -56,116 +48,147 @@ export default class Profile extends Component {
     this.props.handleShowProfile();
     window.scrollTo(0, 0);
   };
+  isPageReady = () => {
+    return this.state.user;
+  };
   render() {
     return (
       <div id="popup">
         <div id="showProfile" className="popupInner">
-          <Row>
-            <Col>
-              <Row>
-                <Col sm={11} />
-                <Col sm={1}>
-                  <span id="closePostPopup" onClick={this.handleClosePopup}>
-                    &nbsp;<i
-                      className="fa fa-times-circle-o"
-                      aria-hidden="true"
-                    />
-                  </span>
-                </Col>
-              </Row>
-              <Row id="profileImage">
-                <Col>
-                  <p>
-                    <img
-                      src={this.state.user.imageSource}
-                      alt="ProfilePicture"
-                    />
-                  </p>
-                </Col>
-              </Row>
-              <Row id="profileName">
-                <Col>
-                  <h5>
-                    <strong>
-                      <u>
-                        {this.state.user.organisationName
-                          ? this.state.user.organisationName
-                          : this.state.user.firstName +
-                            " " +
-                            this.state.user.lastName}
-                      </u>
-                    </strong>
-                  </h5>
-                </Col>
-              </Row>
-              {this.state.user.description ? (
-                <Row id="profileDescription">
-                  <Col sm={3}>
-                    <p>
-                      <u>
-                        {this.state.user.userType === "volunteer"
-                          ? "About Me :"
-                          : "About Us :"}
-                      </u>
-                    </p>
-                  </Col>
-                  <Col sm={9}>
-                    <p>{this.state.user.description}</p>
+          {!this.isPageReady() ? (
+            <Row>
+              <Col xs={4} />
+              <Col xs={4} id="interestRingLoader">
+                <div className="RingLoader center-loading">
+                  <RingLoader
+                    color="#123abc"
+                    size={100} /*the size of the spinner*/
+                  />
+                </div>
+              </Col>
+              <Col xs={4} />
+            </Row>
+          ) : (
+            <Row>
+              <Col>
+                <Row>
+                  <Col sm={11} />
+                  <Col sm={1}>
+                    <span id="closePostPopup" onClick={this.handleClosePopup}>
+                      &nbsp;<i
+                        className="fa fa-times-circle-o"
+                        aria-hidden="true"
+                      />
+                    </span>
                   </Col>
                 </Row>
-              ) : (
-                ""
-              )}
-
-              {this.state.user.interests &&
-              this.state.user.interests.length > 0 ? (
-                <Row id="profileUrl">
-                  <Col sm={3}>
+                <Row id="profileImage">
+                  <Col>
                     <p>
-                      <u>
-                        {this.state.user.userType === "volunteer"
-                          ? "Interested In:"
-                          : "Our Remit:"}
-                      </u>
-                    </p>
-                  </Col>
-
-                  <Col sm={9}>
-                    <p>
-                      <em>
-                        {this.state.user.interests.map((interest, index) => {
-                          if (index == this.state.user.interests.length - 1) {
-                            return interest.name;
-                          } else {
-                            return interest.name + " / ";
-                          }
-                        })}
-                      </em>
+                      <img
+                        src={this.state.user.imageSource}
+                        alt="ProfilePicture"
+                      />
                     </p>
                   </Col>
                 </Row>
-              ) : (
-                ""
-              )}
-              {this.state.user.url ? (
-                <Row id="profileUrl">
-                  <Col sm={3}>
-                    <p>
-                      <u>For more Info:</u>
-                    </p>
-                  </Col>
-
-                  <Col sm={9}>
-                    <a href={this.state.user.url} target="_blank">
-                      {this.state.user.url}
-                    </a>
+                <Row id="profileName">
+                  <Col>
+                    <h5>
+                      <strong>
+                        <u>
+                          {this.state.user.organisationName
+                            ? this.state.user.organisationName
+                            : this.state.user.firstName +
+                              " " +
+                              this.state.user.lastName}
+                        </u>
+                      </strong>
+                    </h5>
                   </Col>
                 </Row>
-              ) : (
-                ""
-              )}
-            </Col>
-          </Row>
+                {this.state.user.description ? (
+                  <Row id="profileDescription">
+                    <Col sm={3}>
+                      <p>
+                        <u>
+                          {this.state.user.userType === "volunteer"
+                            ? "About Me :"
+                            : "About Us :"}
+                        </u>
+                      </p>
+                    </Col>
+                    <Col sm={9}>
+                      <p>{this.state.user.description}</p>
+                    </Col>
+                  </Row>
+                ) : (
+                  ""
+                )}
+
+                {this.state.user.interests &&
+                this.state.user.interests.length > 0 ? (
+                  <Row id="profileUrl">
+                    <Col sm={3}>
+                      <p>
+                        <u>
+                          {this.state.user.userType === "volunteer"
+                            ? "Interested In:"
+                            : "Our Remit:"}
+                        </u>
+                      </p>
+                    </Col>
+
+                    <Col sm={9}>
+                      <p>
+                        <em>
+                          {this.state.user.interests.map((interest, index) => {
+                            if (index == this.state.user.interests.length - 1) {
+                              return interest.name;
+                            } else {
+                              return interest.name + " / ";
+                            }
+                          })}
+                        </em>
+                      </p>
+                    </Col>
+                  </Row>
+                ) : (
+                  ""
+                )}
+                {this.state.user.url ? (
+                  <Row id="profileUrl">
+                    <Col sm={3}>
+                      <p>
+                        <u>For more Info:</u>
+                      </p>
+                    </Col>
+
+                    <Col sm={9}>
+                      <a href={this.state.user.url} target="_blank">
+                        {this.state.user.url}
+                      </a>
+                    </Col>
+                  </Row>
+                ) : (
+                  ""
+                )}
+                {this.state.profileLoadError ? (
+                  <Row>
+                    <Col sm={1} />
+                    <Col sm={10}>
+                      <Alert color="danger">
+                        {this.state.profileLoadError}
+                      </Alert>
+                    </Col>
+                    <Col sm={1} />
+                  </Row>
+                ) : (
+                  ""
+                )}
+              </Col>
+            </Row>
+          )}
         </div>
       </div>
     );
